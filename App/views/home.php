84dc3c2dc87fadd6471b8217797641eb2dfa1f0a
huuthang106 <thangcachep106@gmail.com>
2024-01-13 21:14:15 +0700

    [php] cat giao dien

---
+++ App/views/home.php
@@ -0,0 +1,140 @@
+
+<body>
+   <?php
+    include 'App/views/layouts/navbar.php';
+   ?>
+
+            <!-- Table Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                   
+                    <div class="col-12">
+                        <div class="bg-secondary rounded h-100 p-4">
+                            <h6 class="mb-4">Responsive Table</h6>
+                            <div class="log-entry">
+            <h4 class="log-title">Tiêu đề Nhật Ký 1</h4>
+            <p class="log-content">Nội dung Nhật Ký 1. Đây là một ví dụ về nội dung nhật ký.</p>
+            <a href="#" class="log-details-link">Xem Chi Tiết</a>
+        </div>
+        <div class="log-entry">
+            <h4 class="log-title">Tiêu đề Nhật Ký 1</h4>
+            <p class="log-content">Nội dung Nhật Ký 1. Đây là một ví dụ về nội dung nhật ký.</p>
+            <a href="#" class="log-details-link">Xem Chi Tiết</a>
+        </div>
+        <div class="log-entry">
+            <h4 class="log-title">Tiêu đề Nhật Ký 1</h4>
+            <p class="log-content">Nội dung Nhật Ký 1. Đây là một ví dụ về nội dung nhật ký.</p>
+            <a href="#" class="log-details-link">Xem Chi Tiết</a>
+        </div>
+                            <!-- <div class="table-responsive">
+                                <table class="table" id='myTable'>
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">First Name</th>
+                                            <th scope="col">Last Name</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Country</th>
+                                            <th scope="col">ZIP</th>
+                                            <th scope="col">Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>John</td>
+                                            <td>Doe</td>
+                                            <td>jhon@email.com</td>
+                                            <td>USA</td>
+                                            <td>123</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>mark@email.com</td>
+                                            <td>UK</td>
+                                            <td>456</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                            <td>789</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                            <td>789</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                            <td>789</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                            <td>789</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                            <td>789</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                            <td>789</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                            <td>789</td>
+                                            <td>Member</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Table End -->
+    <?php
+        include 'App/views/layouts/footer.php';
+    ?>
+
+      
+
+</body>
+
